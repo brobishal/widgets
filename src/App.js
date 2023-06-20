577e5e099f0fae9cf9625ba0f2d5d563146461ea
Bishal Shrestha <bbrobishal@gmail.com>
2023-06-20 23:20:46 +0545

    hiding and showing the option list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 const items = [
     {
         title:"What is React ?",
@@ -16,13 +17,41 @@ const items = [
     }
 ];
 
-export default () =>{
-    return (
-        <div>
-            {/* <Accordion items={items}/> */}
-            <Search/>
-        </div>
 
+const options = [
+    {
+        label:'The color red',
+        value:'red'
+    },
+    {
+        label:"The color green",
+        value:"green",
+
+    },
+    {
+        label:'The color blue',
+        value:'blue'
+    }
+
+]
+
+
+export default () =>{
+
+    const [selected, setSelected] = useState(options[0])
+    return (
+        <div> 
+            {/* <Accordion items={items}/> */}
+            {/* <Search/> */}
+            <Dropdown
+//             onSelectedChange: This is the name of a prop that is being passed to a child component. 
+//It suggests that the child component expects a function to be passed as a value for this prop.
+// setSelected: It refers to a function that is being passed as the value for the onSelectedChange prop.
+            onSelectedChange={setSelected}
+             selected={selected} 
+             options={options}/>   
+        </div>
+ 
     )
 }
 
---
+++ src/components/Dropdown.js
@@ -0,0 +1,45 @@
+import React,{useState} from "react";
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  console.log(typeof selected)
+
+
+  const[open, setOpen] = useState(false);
+
+  const renderedOptions = options.map((option) => {
+    // console.log(` value ${option.value} and ${selected.value}`)
+    if(option.value === selected.value) {
+      return null;
+    }
+
+    return (
+      <div 
+      key={option.value} 
+      className="item"
+      onClick={()=> 
+      // we call onSelectedChange function
+      onSelectedChange(option)
+      }
+        >
+      {/* {console.log( onSelectedChange(option))} */}
+      {option.label}
+      </div>
+    );
+
+  });
+
+  
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label"> Select a color</label>
+        <div onClick={()=> setOpen(!open) } className={`ui selection dropdown  ${open ? 'visible active': '' }`}>
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${ open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
