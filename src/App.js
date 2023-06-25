60f7be442c44fdda469ec046b369e07ed2151270
Bishal Shrestha <bbrobishal@gmail.com>
2023-06-25 14:10:22 +0545

    translate widgets starter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 const items = [
     {
         title:"What is React ?",
@@ -37,19 +38,30 @@ const options = [
 
 
 export default () =>{
+    // const [selected, setSelected] = useState(options[0])
+    // const [showDropdown, setShowDropdown] = useState(true);
 
-    const [selected, setSelected] = useState(options[0])
     return (
         <div> 
             {/* <Accordion items={items}/> */}
             {/* <Search/> */}
+
+            {/* <button onClick={()=> setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {
+            showDropdown ?
             <Dropdown
 //             onSelectedChange: This is the name of a prop that is being passed to a child component. 
 //It suggests that the child component expects a function to be passed as a value for this prop.
 // setSelected: It refers to a function that is being passed as the value for the onSelectedChange prop.
             onSelectedChange={setSelected}
              selected={selected} 
-             options={options}/>   
+             options={options}/>  
+             : null 
+            } */}
+
+            <Translate/>
+
+
         </div>
  
     )
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,41 +1,65 @@
-import React,{useState} from "react";
-const Dropdown = ({ options, selected, onSelectedChange }) => {
-  console.log(typeof selected)
+import React, { useState, useEffect, useRef } from "react";
 
+const Dropdown = ({label, options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
 
-  const[open, setOpen] = useState(false);
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      // console.log(event.target);
+      // console.log('Body click');
+      if (ref.current && ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+
+    // when first render our component it runs one time
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
 
   const renderedOptions = options.map((option) => {
     // console.log(` value ${option.value} and ${selected.value}`)
-    if(option.value === selected.value) {
+    if (option.value === selected.value) {
       return null;
     }
 
     return (
-      <div 
-      key={option.value} 
-      className="item"
-      onClick={()=> 
-      // we call onSelectedChange function
-      onSelectedChange(option)
-      }
-        >
-      {/* {console.log( onSelectedChange(option))} */}
-      {option.label}
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          // we call onSelectedChange function
+          // console.log("Item clciked");
+          onSelectedChange(option);
+        }}
+      >
+        {/* {console.log( onSelectedChange(option))} */}
+        {option.label}
       </div>
     );
-
   });
 
-  
   return (
-    <div className="ui form">
+    // Use useRef to track application renders.
+    <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label"> Select a color</label>
-        <div onClick={()=> setOpen(!open) } className={`ui selection dropdown  ${open ? 'visible active': '' }`}>
+        <label className="label">{label}</label>
+        <div
+          onClick={() => {
+            // console.log("Dropdown clciked");
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown  ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${ open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
